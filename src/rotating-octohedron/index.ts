1938774fbed7a4241cebcab527670fa9db94dba9
Charles Taylor <charlestaylor95@gmail.com>
2023-11-18 23:47:47 -0500

    WIP

--- src/rotating-octohedron/index.ts
+++ src/rotating-octohedron/index.ts
@@ -128,33 +128,26 @@ function setColorAttribute(
   gl.enableVertexAttribArray(vertexColor);
 }
 
+// prettier-ignore
 function initPositionBuffer(gl: WebGLRenderingContext): WebGLBuffer {
   const positionBuffer = gl.createBuffer()!;
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  const positions = [
-    // Front face
-    -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
+  const v = [-1, 1];
 
-    // Back face
-    -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
+  let positions: number[] = [];
+  for (let x of v) {
+    for (let y of v) {
+      for (let z of v) {
+        positions.push(
+          x, 0, 0,
+          0, y, 0,
+          0, 0, z,
+        )
+      }
+    }
+  }
 
-    // Top face
-    -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
-
-    // Bottom face
-    -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
-
-    // Right face
-    1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
-
-    // Left face
-    -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
-  ];
-
-  // Now pass the list of positions into WebGL to build the
-  // shape. We do this by creating a Float32Array from the
-  // JavaScript array, then use it to fill the current buffer.
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   return positionBuffer;
@@ -162,12 +155,14 @@ function initPositionBuffer(gl: WebGLRenderingContext): WebGLBuffer {
 
 export function initColorBuffer(gl: WebGLRenderingContext): WebGLBuffer {
   const faceColors = [
-    [1.0, 1.0, 1.0, 1.0], // Front face: white
-    [1.0, 0.0, 0.0, 1.0], // Back face: red
-    [0.0, 1.0, 0.0, 1.0], // Top face: green
-    [0.0, 0.0, 1.0, 1.0], // Bottom face: blue
-    [1.0, 1.0, 0.0, 1.0], // Right face: yellow
-    [1.0, 0.0, 1.0, 1.0], // Left face: purple
+    [1.0, 1.0, 1.0, 1.0], //  white
+    [1.0, 0.0, 0.0, 1.0], //  red
+    [0.0, 1.0, 0.0, 1.0], //  green
+    [0.0, 0.0, 1.0, 1.0], //  blue
+    [1.0, 1.0, 0.0, 1.0], //  yellow
+    [1.0, 0.0, 1.0, 1.0], //  purple
+    [0.0, 1.0, 1.0, 1.0], //  cyan
+    [0.5, 0.5, 0.5, 1.0], //  grey
   ];
 
   // Convert the array of colors into a table for all the vertices.
@@ -187,54 +182,22 @@ export function initColorBuffer(gl: WebGLRenderingContext): WebGLBuffer {
   return colorBuffer;
 }
 
-export function initIndexBuffer(gl: WebGLRenderingContext): WebGLBuffer {
+// prettier-ignore
+function initIndexBuffer(gl: WebGLRenderingContext): WebGLBuffer {
   const indexBuffer = gl.createBuffer()!;
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 
-  // This array defines each face as two triangles, using the
-  // indices into the vertex array to specify each triangle's
-  // position.
-
   const indices = [
-    0,
-    1,
-    2,
-    0,
-    2,
-    3, // front
-    4,
-    5,
-    6,
-    4,
-    6,
-    7, // back
-    8,
-    9,
-    10,
-    8,
-    10,
-    11, // top
-    12,
-    13,
-    14,
-    12,
-    14,
-    15, // bottom
-    16,
-    17,
-    18,
-    16,
-    18,
-    19, // right
-    20,
-    21,
-    22,
-    20,
-    22,
-    23, // left
+    0, 1, 2,
+    3, 4, 5,
+    6, 7, 8,
+    9, 10, 11,
+    12, 13, 14,
+    15, 16, 17,
+    18, 19, 20,
+    21, 22, 23,
   ];
 
-  // Now send the element array to GL
 
   gl.bufferData(
     gl.ELEMENT_ARRAY_BUFFER,
